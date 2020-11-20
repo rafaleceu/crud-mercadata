4d2f7bb8424e02da7594d5c9df3ca3023283dd23
021dev <rafa021dev@gmail.com>
2020-11-19 22:49:27 -0300

    <refact>: change UserCreate imports

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {  BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import userAddComponent from'./components/userAdd.component';
-import UserListComponent from './components/userList.component';
-import UserUpdate from './components/userUpdate.component';
-import UserDelete from './components/userDelete.component';
+import UserCreateComponent from'./components/UserCreate.component';
+import UserListComponent from './components/UserList.component';
+import UserUpdateComponent from './components/UserUpdate.component';
+import UserDeleteComponent from './components/UserDelete.component';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div>
+
                     <nav className="navbar navbar-expand navbar-dark bg-dark">
                         <a href="/" className="navbar-brand">CRUD</a>
                         <div className="navbar-nav mr-auto">
+
                             <li className="nav-item">
                             <Link to={"/create"} className="nav-link">Create</Link>
                             </li>
@@ -31,22 +33,23 @@ class App extends Component {
                             <li className="nav-item">
                             <Link to={"/delete"} className="nav-link">Delete</Link>
                             </li>
+
                         </div>
                     </nav>
 
                     <div className="container mt-3">
                         <Switch>
-                            <Route exact path = { ["/create"] } component={userAddComponent}    />
-                            <Route exact path = { ["/read"]   } component={UserListComponent}   />
-                            <Route exact path = { ["/update"] } component={UserUpdate} />
-                            <Route exact path = { ["/delete"] } component={UserDelete} />
+                            <Route exact path = { ["/create"] } component={UserCreateComponent} />
+                            <Route exact path = { ["/read"]   } component={UserListComponent} />
+                            <Route exact path = { ["/update"] } component={UserUpdateComponent} />
+                            <Route exact path = { ["/delete"] } component={UserDeleteComponent} />
                         </Switch>
                     </div>
+
                 </div>
             </Router>
         );
     }
 }
-
 
 export default App;
