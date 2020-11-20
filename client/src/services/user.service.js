b6c708370af7ffa1f86b8668d64ca26a58dd830c
021dev <rafa021dev@gmail.com>
2020-11-19 22:50:10 -0300

    <refact> update patch http request from user services

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -4,7 +4,7 @@ const usersList = () => http.get('/api');
 const oneUser = (id) => http.get(`/api/${id}`);
 const userAdd = (userForm) => http.post(`/api`, userForm);
 const userDelete = (id) => http.delete(`/api/${id}`);
-const userUpdate = (id) => http.patch(`/api/${id}`);
+const userUpdate = (id, userForm) => http.patch(`/api/${id}`, userForm);
 
 export {
     usersList,
